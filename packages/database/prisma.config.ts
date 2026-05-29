2f656b9a70d093b39a0eaaf96c54ba12b22f8908
marshot9420 <marshot9420@gmail.com>
2026-05-29 15:37:05 +0900

    configs: database 패키지 기본 설정

---
+++ packages/database/prisma.config.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import path from "node:path";
+
+import { defineConfig, env } from "prisma/config";
+
+dotenv.config({
+  path: path.resolve(process.cwd(), "../../.env"),
+});
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+
+  migrations: {
+    path: "prisma/migrations",
+  },
+
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
