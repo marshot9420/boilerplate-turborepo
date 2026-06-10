fb7ef4ad47ec1575a80bdaabbfe0a4f3b13f3499
marshot9420 <marshot9420@gmail.com>
2026-06-10 15:24:45 +0900

    configs: 앱 RootLayout에 Brave 브라우저를 위한 설정 추가

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -11,9 +11,15 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const isDev = process.env.NODE_ENV !== "production";
+
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body
+        {...(isDev ? { suppressHydrationWarning: true } : {})} // Brave 브라우저에서 발생하는 개발 환경 문제 (`cz-shortcut-listen="true"` 주입 문제)
+      >
+        {children}
+      </body>
     </html>
   );
 }
